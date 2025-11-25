b4ba2e4b6c239744469d06d5944ee8bae4d82cee
kittsjs <46006020+kittsjs@users.noreply.github.com>
2025-11-26 00:15:32 +0530

    Changed the url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'https://webscraper-3-wl4p.onrender.com';
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'https://webscraper-3-wl4p.onrender.com';
+// export const API_BASE_URL = 'http://localhost:3000';
 export const API_ENDPOINTS = {
   EXTRACT_IMAGES: '/api/extract-images',
 } as const;
